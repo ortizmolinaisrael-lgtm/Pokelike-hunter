c701b6b764289ad761f1abf50078a500839f3974
ortizmolinaisrael-lgtm <ortizmolinaisrael@gmail.com>
2026-06-26 10:13:00 +0200

    Bump version to 1.2 and update script name
    Updated version number and name in the user script.

--- pokelike-hunter.user.js
+++ pokelike-hunter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         PokéLike Hunter 1.0
+// @name         PokéLike Hunter
 // @namespace    https://github.com/ortizmolinaisrael-lgtm/pokelike-hunter
-// @version      1.1
+// @version		 1.0 → 1.1 → 1.2
 // @description  Auto hunter PokéLike
 // @author       Israfix
 // @match        *://*/*
@@ -38,7 +38,7 @@
 `;
 
     panel.innerHTML = `
-        <b>🎯 PokéLike Hunter V1.1</b>
+        <b>🎯 PokéLike Hunter V1.2</b>
 
         <textarea id="pk_list" style="width:100%;height:80px;"></textarea>
 
